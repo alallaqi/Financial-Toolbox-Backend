7e5d226769984ccedace07d836fd93a58142b2e9
alallaqi <abdelrahman.alallaqi@powercoders.org>
2024-04-10 22:59:33 +0200

    Set up basic Express server, testing framework, and README

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Financial Toolbox Backend is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+module.exports = app; // Export for testing
